684f7a5bba7c0d83bf126061f32ab0632cb5dd8b
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2023-03-08 20:43:10 +0000

    add email and author to setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'stockplot',
-    version = '1.0.0',
     packages = find_packages(),
+    email='thomasmcinally@hotmail.com',
+    author='Thomas Mcinally',
     install_requires=[
         'matplotlib',
         'pandas',
         'yfinance',
         'mplfinance'
     ],  
+    version = '1.0.0',
     entry_points = {
         'console_scripts': [
             'stockplot = stockplot.main:main'
