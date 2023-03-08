d4791283c98e80303e42e2552341841ff2fbf49c
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2023-03-08 20:46:46 +0000

    add long_description_content_type to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     packages = find_packages(),
     email='thomasmcinally@hotmail.com',
     author='Thomas Mcinally',
+    long_description_content_type="text/markdown",
     install_requires=[
         'matplotlib',
         'pandas',
