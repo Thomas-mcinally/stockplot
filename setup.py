25aa269923fd346d1019544424ef53a00804170c
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2023-03-09 23:18:51 +0000

    increment version to test new github action

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
         "yfinance==0.2.12",
         "zipp==3.15.0; python_version < '3.10'",
     ],
-    version="1.0.11",
+    version="1.0.12",
     entry_points={"console_scripts": ["stockplot = stockplot.main:main"]},
 )
